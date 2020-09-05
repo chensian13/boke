3d481f581204cd9264212a4200de427e095f0022
dpchensian <dpchensian@dfl.com.cn>
2020-09-05 10:32:53 +0800

    修改公共模块

--- front/src/main/java/csa/stu/app/front/component/ServerCorsFilter.java
+++ front/src/main/java/csa/stu/app/front/component/ServerCorsFilter.java
@@ -1,6 +1,6 @@
 package csa.stu.app.front.component;
 
-import csa.stu.util.components.aop.CorsFilter;
+import csa.stu.util.ap.web_helper.cors.CorsFilter;
 import org.springframework.stereotype.Component;
 
 @Component
